cleech/open-isns

commit 27048ab83f1f1bd9282bcbf5f8f4358fe2da52e1

Change isns_portal_string() to return allocated string.
This eliminates possible string overrun problems with using
a static string, but it means that we have to free the string
every place it's used. But this makes gcc7 happy.

attrs.c
@@ -1042,18 +1042,21 @@ isns_portal_to_attr_list(const isns_portal_info_t *portal,
 	return 1;
 }
 
-const char *
+char *
 isns_portal_string(const isns_portal_info_t *portal)
 {
 	const struct sockaddr_in6 *six = &portal->addr;
-	static char	buffer[128];
+	char		*bufp;
 	char		abuf[128];
 
 	inet_ntop(six->sin6_family, &six->sin6_addr, abuf, sizeof(abuf));
-	snprintf(buffer, sizeof(buffer), "[%s]:%d/%s",
+	if (asprintf(&bufp, "[%s]:%d/%s",
 			abuf, ntohs(six->sin6_port),
-			(portal->proto == IPPROTO_UDP)? "udp" : "tcp");
-	return buffer;
+			(portal->proto == IPPROTO_UDP)? "udp" : "tcp") < 0) {
+		isns_error("Error printing portal: %m\n");
+		return NULL;
+	}
+	return bufp;
 }
 
 int

dd.c
@@ -1239,7 +1239,7 @@ isns_dd_get_member_object(isns_db_t *db, const isns_attr_t *key1,
 	isns_object_template_t *tmpl = NULL;
 	isns_object_t	*obj;
 	isns_portal_info_t portal_info;
-	const char	*key_string = NULL;
+	char		*key_string = NULL;
 	uint32_t	key_index = 0;
 
 	switch (key1->ia_tag_id) {
@@ -1309,6 +1309,8 @@ isns_dd_get_member_object(isns_db_t *db, const isns_attr_t *key1,
 
 out:
 	isns_attr_list_destroy(&query);
+	if (key_string)
+		free(key_string);
 	return obj;
 
 }

esi.c
@@ -123,8 +123,11 @@ isns_esi_transmit(void *ptr)
 
 			if (esp->esp_next_xmit <= now) {
 				if (esp->esp_retries == 0) {
+					char *ps = isns_portal_string(&esp->esp_dest);
+
 					isns_debug_esi("No ESI response from %s - dropping\n",
-							isns_portal_string(&esp->esp_dest));
+							ps);
+					free(ps);
 					isns_esi_drop_portal(esp, db, 1);
 					continue;
 				}
@@ -154,6 +157,7 @@ isns_esi_sendto(isns_esi_t *esi, isns_esi_portal_t *esp)
 	isns_attr_list_t attrs = ISNS_ATTR_LIST_INIT;
 	isns_socket_t	*sock;
 	isns_simple_t	*msg;
+	char		*ps = NULL;
 
 	/* For TCP portals, kill the TCP socket every time. */
 	if (esp->esp_dest.proto == IPPROTO_TCP)
@@ -185,9 +189,10 @@ isns_esi_sendto(isns_esi_t *esi, isns_esi_portal_t *esp)
 	if (msg == NULL)
 		return;
 
+	ps = isns_portal_string(&esp->esp_dest);
 	isns_debug_esi("*** Sending ESI message to %s (xid=0x%x); %u retries left\n",
-			isns_portal_string(&esp->esp_dest),
-			msg->is_xid, esp->esp_retries);
+			ps, msg->is_xid, esp->esp_retries);
+	free(ps);
 	isns_simple_transmit(esp->esp_socket, msg,
 			NULL, esp->esp_timeout - 1,
 			isns_process_esi_response);
@@ -241,6 +246,7 @@ isns_esi_update(isns_esi_t *esi)
 	ISNS_LIST_DECLARE(hold);
 	isns_esi_portal_t *esp;
 	unsigned int	i;
+	char		*ps = NULL;
 
 	isns_debug_esi("Updating ESI state for entity %u\n", entity->ie_index);
 
@@ -272,15 +278,19 @@ isns_esi_update(isns_esi_t *esi)
 			esp = isns_list_item(isns_esi_portal_t, esp_list, pos);
 
 			if (esp->esp_object == child) {
+				ps = isns_portal_string(&portal_info);
 				isns_debug_esi("Updating ESI state for %s\n",
-						isns_portal_string(&portal_info));
+						ps);
+				free(ps);
 				isns_list_del(&esp->esp_list);
 				goto update;
 			}
 		}
 
+		ps = isns_portal_string(&portal_info);
 		isns_debug_esi("Creating ESI state for %s\n",
-				isns_portal_string(&portal_info));
+				ps);
+		free(ps);
 		esp = isns_calloc(1, sizeof(*esp));
 		esp->esp_object = isns_object_get(child);
 		isns_list_init(&esp->esp_list);
@@ -368,8 +378,10 @@ __isns_esi_drop_object(isns_db_t *db, isns_object_t *obj, unsigned int dead)
 void
 isns_esi_drop_portal(isns_esi_portal_t *esp, isns_db_t *db, int dead)
 {
-	isns_debug_esi("ESI: dropping portal %s\n",
-			isns_portal_string(&esp->esp_portal));
+	char	*ps = isns_portal_string(&esp->esp_portal);
+
+	isns_debug_esi("ESI: dropping portal %s\n", ps);
+	free(ps);
 
 	isns_list_del(&esp->esp_list);
 	isns_esi_disconnect(esp);
@@ -472,6 +484,7 @@ isns_process_esi_response(uint32_t xid, int status, isns_simple_t *msg)
 	isns_portal_info_t	portal_info;
 	isns_esi_portal_t	*esp;
 	isns_esi_t		*esi;
+	char			*ps;
 
 	if (msg == NULL) {
 		isns_debug_esi("ESI call 0x%x timed out\n", xid);
@@ -498,13 +511,16 @@ isns_process_esi_response(uint32_t xid, int status, isns_simple_t *msg)
 	}
 
 	if (!isns_portal_equal(&esp->esp_portal, &portal_info)) {
+		ps = isns_portal_string(&portal_info);
 		isns_warning("Faked ESI response for portal %s\n",
-				isns_portal_string(&portal_info));
+				ps);
+		free(ps);
 		return;
 	}
 
-	isns_debug_esi("Good ESI response from %s\n",
-				isns_portal_string(&portal_info));
+	ps = isns_portal_string(&portal_info);
+	isns_debug_esi("Good ESI response from %s\n", ps);
+	free(ps);
 	isns_esi_restart(esp);
 
 	/* Refresh the entity's registration timestamp */

include/libisns/isns.h
@@ -654,7 +654,7 @@ extern int		isns_portal_to_object(const isns_portal_info_t *,
 				isns_object_t *);
 extern int		isns_portal_is_wildcard(const isns_portal_info_t *);
 extern uint32_t		isns_portal_tcpudp_port(const isns_portal_info_t *);
-extern const char *	isns_portal_string(const isns_portal_info_t *);
+extern char *		isns_portal_string(const isns_portal_info_t *);
 extern int		isns_portal_equal(const isns_portal_info_t *,
 				const isns_portal_info_t *);
 extern int		isns_enumerate_portals(isns_portal_info_t *,

isnsdd.c
@@ -395,6 +395,7 @@ check_portal_registration(void *ptr)
 		isns_proxy_t	*proxy;
 		time_t		last_modified;
 		uint32_t	interval;
+		char		*ps;
 
 		if (!isns_object_get_uint32(obj, ISNS_TAG_ESI_INTERVAL, &interval))
 			continue;
@@ -410,10 +411,10 @@ check_portal_registration(void *ptr)
 				ISNS_TAG_PORTAL_TCP_UDP_PORT,
 				obj);
 
+		ps = isns_portal_string(&portal_info),
 		isns_notice("Portal %s did not receive ESIs within %u seconds - "
 			"server may have lost us.\n",
-			isns_portal_string(&portal_info),
-			now - last_modified);
+			ps, now - last_modified);
 
 		proxy = isns_proxy_by_entity(isns_object_get_entity(obj));
 		if (!proxy)

scn.c
@@ -699,6 +699,7 @@ isns_scn_get_socket(isns_scn_t *scn)
 	isns_scn_funnel_t *f, *best = NULL;
 	isns_socket_t	*sock;
 	unsigned int	worst = 0, loops = 0, nfunnels;
+	char		*ps;
 
 	/* Keep it simple for now */
 	if ((f = scn->scn_current_funnel) != NULL && f->scn_socket) {
@@ -737,9 +738,9 @@ again:
 	isns_socket_set_security_ctx(sock,
 			isns_default_security_context(1));
 
+	ps = isns_portal_string(&best->scn_portal);
 	isns_debug_scn("SCN: %s using portal %s\n",
-			scn->scn_name,
-			isns_portal_string(&best->scn_portal));
+			scn->scn_name, ps);
 	scn->scn_current_funnel = best;
 	best->scn_socket = sock;
 	return sock;

socket.c
@@ -2363,6 +2363,7 @@ isns_socket_get_portal_info(const isns_socket_t *sock,
 	struct sockaddr_storage addr;
 	socklen_t	alen;
 	int		fd, success = 0;
+	char		*ps;
 
 	memset(portal, 0, sizeof(*portal));
 
@@ -2394,7 +2395,9 @@ isns_socket_get_portal_info(const isns_socket_t *sock,
 	else
 		portal->proto = IPPROTO_UDP;
 
-	debug_verbose("socket_get_portal: %s\n", isns_portal_string(portal));
+	ps = isns_portal_string(portal);
+	debug_verbose("socket_get_portal: %s\n", ps);
+	free(ps);
 	success = 1;
 
 out:

sysdep-unix.c
@@ -7,6 +7,7 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <libisns/isns.h>
 #include <libisns/util.h>
@@ -92,6 +93,7 @@ isns_enumerate_portals(isns_portal_info_t *result, unsigned int max)
 		struct sockaddr_storage ifaddr;
 		isns_portal_info_t portal;
 		int		ifflags;
+		char		*ps;
 
 		memcpy(&ifr, ptr, sizeof(ifr));
 		ptr += sizeof(ifr);
@@ -114,9 +116,10 @@ isns_enumerate_portals(isns_portal_info_t *result, unsigned int max)
 		if (!isns_portal_from_sockaddr(&portal, &ifaddr))
 			continue;
 
+		ps = isns_portal_string(&portal);
 		isns_debug_socket("Got interface %u: %s %s\n",
-				nportals, ifr.ifr_name,
-				isns_portal_string(&portal));
+				nportals, ifr.ifr_name, ps);
+		free(ps);
 		if (nportals < max)
 			result[nportals++] = portal;
 	}

commit 98328d45ecd03797763a9d436c9c720fbfd12342

Change isns_portal_string() to use static mem
An earlier fix to isns_portal_string() caused problems
because it changed the return value from pointing to
const (static) memory, which introduced the need to
free the memory every place this helper function was
called, some of which were done incorrectly. Simplify
things by restoring isns_port_string() to again return
a pointer to static memory, fixing the original
possible-overflow problem.

Fixes: 27048ab83f1f1bd9282bcbf5f8f4358fe2da52e1

attrs.c
@@ -1042,21 +1042,19 @@ isns_portal_to_attr_list(const isns_portal_info_t *portal,
 	return 1;
 }
 
-char *
+static char portal_buf[1024];
+
+const char *
 isns_portal_string(const isns_portal_info_t *portal)
 {
 	const struct sockaddr_in6 *six = &portal->addr;
-	char		*bufp;
 	char		abuf[128];
 
 	inet_ntop(six->sin6_family, &six->sin6_addr, abuf, sizeof(abuf));
-	if (asprintf(&bufp, "[%s]:%d/%s",
+	snprintf(portal_buf, sizeof(portal_buf), "[%s]:%d/%s",
 			abuf, ntohs(six->sin6_port),
-			(portal->proto == IPPROTO_UDP)? "udp" : "tcp") < 0) {
-		isns_error("Error printing portal: %m\n");
-		return NULL;
-	}
-	return bufp;
+			(portal->proto == IPPROTO_UDP)? "udp" : "tcp");
+	return portal_buf;
 }
 
 int

dd.c
@@ -1286,7 +1286,7 @@ isns_dd_get_member_object(isns_db_t *db, const isns_attr_t *key1,
 			 &query))
 			return NULL;
 
-		key_string = isns_portal_string(&portal_info);
+		key_string = (char *)isns_portal_string(&portal_info);
 		tmpl = &isns_portal_template;
 		break;
 
@@ -1312,8 +1312,6 @@ isns_dd_get_member_object(isns_db_t *db, const isns_attr_t *key1,
 
 out:
 	isns_attr_list_destroy(&query);
-	if (key_string)
-		free(key_string);
 	return obj;
 
 }

esi.c
@@ -123,11 +123,8 @@ isns_esi_transmit(__attribute__((unused))void *ptr)
 
 			if (esp->esp_next_xmit <= now) {
 				if (esp->esp_retries == 0) {
-					char *ps = isns_portal_string(&esp->esp_dest);
-
 					isns_debug_esi("No ESI response from %s - dropping\n",
-							ps);
-					free(ps);
+							isns_portal_string(&esp->esp_dest));
 					isns_esi_drop_portal(esp, db, 1);
 					continue;
 				}
@@ -157,7 +154,6 @@ isns_esi_sendto(isns_esi_t *esi, isns_esi_portal_t *esp)
 	isns_attr_list_t attrs = ISNS_ATTR_LIST_INIT;
 	isns_socket_t	*sock;
 	isns_simple_t	*msg;
-	char		*ps = NULL;
 
 	/* For TCP portals, kill the TCP socket every time. */
 	if (esp->esp_dest.proto == IPPROTO_TCP)
@@ -189,10 +185,9 @@ isns_esi_sendto(isns_esi_t *esi, isns_esi_portal_t *esp)
 	if (msg == NULL)
 		return;
 
-	ps = isns_portal_string(&esp->esp_dest);
 	isns_debug_esi("*** Sending ESI message to %s (xid=0x%x); %u retries left\n",
-			ps, msg->is_xid, esp->esp_retries);
-	free(ps);
+			isns_portal_string(&esp->esp_dest),
+			msg->is_xid, esp->esp_retries);
 	isns_simple_transmit(esp->esp_socket, msg,
 			NULL, esp->esp_timeout - 1,
 			isns_process_esi_response);
@@ -246,7 +241,6 @@ isns_esi_update(isns_esi_t *esi)
 	ISNS_LIST_DECLARE(hold);
 	isns_esi_portal_t *esp;
 	unsigned int	i;
-	char		*ps = NULL;
 
 	isns_debug_esi("Updating ESI state for entity %u\n", entity->ie_index);
 
@@ -278,19 +272,15 @@ isns_esi_update(isns_esi_t *esi)
 			esp = isns_list_item(isns_esi_portal_t, esp_list, pos);
 
 			if (esp->esp_object == child) {
-				ps = isns_portal_string(&portal_info);
 				isns_debug_esi("Updating ESI state for %s\n",
-						ps);
-				free(ps);
+						isns_portal_string(&portal_info));
 				isns_list_del(&esp->esp_list);
 				goto update;
 			}
 		}
 
-		ps = isns_portal_string(&portal_info);
 		isns_debug_esi("Creating ESI state for %s\n",
-				ps);
-		free(ps);
+				isns_portal_string(&portal_info));
 		esp = isns_calloc(1, sizeof(*esp));
 		esp->esp_object = isns_object_get(child);
 		isns_list_init(&esp->esp_list);
@@ -378,10 +368,9 @@ __isns_esi_drop_object(isns_db_t *db, isns_object_t *obj, unsigned int dead)
 void
 isns_esi_drop_portal(isns_esi_portal_t *esp, isns_db_t *db, int dead)
 {
-	char	*ps = isns_portal_string(&esp->esp_portal);
 
-	isns_debug_esi("ESI: dropping portal %s\n", ps);
-	free(ps);
+	isns_debug_esi("ESI: dropping portal %s\n",
+			isns_portal_string(&esp->esp_portal));
 
 	isns_list_del(&esp->esp_list);
 	isns_esi_disconnect(esp);
@@ -486,7 +475,6 @@ isns_process_esi_response(uint32_t xid,
 	isns_portal_info_t	portal_info;
 	isns_esi_portal_t	*esp;
 	isns_esi_t		*esi;
-	char			*ps;
 
 	if (msg == NULL) {
 		isns_debug_esi("ESI call 0x%x timed out\n", xid);
@@ -513,16 +501,13 @@ isns_process_esi_response(uint32_t xid,
 	}
 
 	if (!isns_portal_equal(&esp->esp_portal, &portal_info)) {
-		ps = isns_portal_string(&portal_info);
 		isns_warning("Faked ESI response for portal %s\n",
-				ps);
-		free(ps);
+				isns_portal_string(&portal_info));
 		return;
 	}
 
-	ps = isns_portal_string(&portal_info);
-	isns_debug_esi("Good ESI response from %s\n", ps);
-	free(ps);
+	isns_debug_esi("Good ESI response from %s\n",
+			isns_portal_string(&portal_info));
 	isns_esi_restart(esp);
 
 	/* Refresh the entity's registration timestamp */

include/libisns/isns.h
@@ -654,7 +654,7 @@ extern int		isns_portal_to_object(const isns_portal_info_t *,
 				isns_object_t *);
 extern int		isns_portal_is_wildcard(const isns_portal_info_t *);
 extern uint32_t		isns_portal_tcpudp_port(const isns_portal_info_t *);
-extern char *		isns_portal_string(const isns_portal_info_t *);
+extern const char *	isns_portal_string(const isns_portal_info_t *);
 extern int		isns_portal_equal(const isns_portal_info_t *,
 				const isns_portal_info_t *);
 extern int		isns_enumerate_portals(isns_portal_info_t *,

isnsdd.c
@@ -396,7 +396,6 @@ check_portal_registration(__attribute__((unused))void *ptr)
 		isns_proxy_t	*proxy;
 		time_t		last_modified;
 		uint32_t	interval;
-		char		*ps;
 
 		if (!isns_object_get_uint32(obj, ISNS_TAG_ESI_INTERVAL, &interval))
 			continue;
@@ -412,10 +411,10 @@ check_portal_registration(__attribute__((unused))void *ptr)
 				ISNS_TAG_PORTAL_TCP_UDP_PORT,
 				obj);
 
-		ps = isns_portal_string(&portal_info),
 		isns_notice("Portal %s did not receive ESIs within %u seconds - "
 			"server may have lost us.\n",
-			ps, now - last_modified);
+			isns_portal_string(&portal_info),
+			now - last_modified);
 
 		proxy = isns_proxy_by_entity(isns_object_get_entity(obj));
 		if (!proxy)

scn.c
@@ -700,7 +700,6 @@ isns_scn_get_socket(isns_scn_t *scn)
 	isns_scn_funnel_t *f, *best = NULL;
 	isns_socket_t	*sock;
 	unsigned int	worst = 0, loops = 0, nfunnels;
-	char		*ps;
 
 	/* Keep it simple for now */
 	if ((f = scn->scn_current_funnel) != NULL && f->scn_socket) {
@@ -739,9 +738,9 @@ again:
 	isns_socket_set_security_ctx(sock,
 			isns_default_security_context(1));
 
-	ps = isns_portal_string(&best->scn_portal);
 	isns_debug_scn("SCN: %s using portal %s\n",
-			scn->scn_name, ps);
+			scn->scn_name,
+			isns_portal_string(&best->scn_portal));
 	scn->scn_current_funnel = best;
 	best->scn_socket = sock;
 	return sock;

socket.c
@@ -2370,7 +2370,6 @@ isns_socket_get_portal_info(const isns_socket_t *sock,
 	struct sockaddr_storage addr;
 	socklen_t	alen;
 	int		fd, success = 0;
-	char		*ps;
 
 	memset(portal, 0, sizeof(*portal));
 
@@ -2402,9 +2401,8 @@ isns_socket_get_portal_info(const isns_socket_t *sock,
 	else
 		portal->proto = IPPROTO_UDP;
 
-	ps = isns_portal_string(portal);
-	debug_verbose("socket_get_portal: %s\n", ps);
-	free(ps);
+	debug_verbose("socket_get_portal: %s\n",
+			isns_portal_string(portal));
 	success = 1;
 
 out:

sysdep-unix.c
@@ -93,7 +93,6 @@ isns_enumerate_portals(isns_portal_info_t *result, unsigned int max)
 		struct sockaddr_storage ifaddr;
 		isns_portal_info_t portal;
 		int		ifflags;
-		char		*ps;
 
 		memcpy(&ifr, ptr, sizeof(ifr));
 		ptr += sizeof(ifr);
@@ -116,10 +115,9 @@ isns_enumerate_portals(isns_portal_info_t *result, unsigned int max)
 		if (!isns_portal_from_sockaddr(&portal, &ifaddr))
 			continue;
 
-		ps = isns_portal_string(&portal);
 		isns_debug_socket("Got interface %u: %s %s\n",
-				nportals, ifr.ifr_name, ps);
-		free(ps);
+				nportals, ifr.ifr_name,
+				isns_portal_string(&portal));
 		if (nportals < max)
 			result[nportals++] = portal;
 	}