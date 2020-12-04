cleech/open-isns

commit 86bf736873ed00b601c4b3abf33975d8eb5d8077

General cleanup for the compiler.
Using gcc 9.2.1, and adding in "-Wextra" found a bunch of things
that needed cleaning up:
 * many unused parameters
 * a bunch of signed-vs-unsigned comparisons
 * a few "fallthrough" cases not correctly marked
 * a struct array partion initialization that didn't always use tags
 * renamed usage() to usage_and_exit() to be clear
 * some duplicate struct initialzation tags/values
 * a couple of funky "else if" formats
 * make SOCK_DEBUG_VERBOSE actually work with a fwd declaration
 * fixed a couple of switch statements without a default case

 Should be no functional change.

pki.c
@@ -155,7 +155,7 @@ isns_dsasig_report_errors(const char *msg, isns_print_fn_t *fn)
 }
 
 int
-isns_dsasig_sign(isns_security_t *ctx,
+isns_dsasig_sign(__attribute__((unused))isns_security_t *ctx,
 			isns_principal_t *peer,
 			buf_t *pdu,
 			struct isns_authblk *blk)
@@ -176,7 +176,7 @@ isns_dsasig_sign(isns_security_t *ctx,
 			peer->is_name);
 		return 0;
 	}
-	if (EVP_PKEY_size(pkey) > sizeof(signature)) {
+	if (EVP_PKEY_size(pkey) > (int)sizeof(signature)) {
 		isns_error("isns_dsasig_sign: signature buffer too small\n");
 		return 0;
 	}
@@ -208,7 +208,7 @@ isns_dsasig_sign(isns_security_t *ctx,
 }
 
 int
-isns_dsasig_verify(isns_security_t *ctx,
+isns_dsasig_verify(__attribute__((unused))isns_security_t *ctx,
 			isns_principal_t *peer,
 			buf_t *pdu,
 			const struct isns_authblk *blk)
@@ -250,7 +250,8 @@ isns_dsasig_verify(isns_security_t *ctx,
 }
 
 EVP_PKEY *
-isns_dsasig_load_private_pem(isns_security_t *ctx, const char *filename)
+isns_dsasig_load_private_pem(__attribute__((unused))isns_security_t *ctx,
+		const char *filename)
 {
 	EVP_PKEY	*pkey;
 	FILE		*fp;
@@ -267,7 +268,8 @@ isns_dsasig_load_private_pem(isns_security_t *ctx, const char *filename)
 }
 
 EVP_PKEY *
-isns_dsasig_load_public_pem(isns_security_t *ctx, const char *filename)
+isns_dsasig_load_public_pem(__attribute__((unused))isns_security_t *ctx,
+		const char *filename)
 {
 	EVP_PKEY	*pkey;
 	FILE		*fp;
@@ -429,8 +431,10 @@ isns_dsa_load_params(const char *filename)
 	return dsa;
 }
 
-static void
-isns_dsa_param_gen_callback(int stage, int index, void *dummy)
+static int
+isns_dsa_param_gen_callback(int stage,
+		__attribute__((unused))int index,
+		__attribute__((unused))void *dummy)
 {
 	if (stage == 0)
 		write(1, "+", 1);
@@ -438,6 +442,7 @@ isns_dsa_param_gen_callback(int stage, int index, void *dummy)
 		write(1, ".", 1);
 	else if (stage == 2)
 		write(1, "/", 1);
+	return 0;
 }
 
 int

attrs.c
@@ -1131,25 +1131,29 @@ isns_portal_parse(isns_portal_info_t *portal,
  * Attribute type NIL
  */
 static int
-isns_attr_type_nil_encode(buf_t *bp, const isns_value_t *value)
+isns_attr_type_nil_encode(buf_t *bp,
+		__attribute__((unused))const isns_value_t *value)
 {
 	return buf_put32(bp, 0);
 }
 
 static int
-isns_attr_type_nil_decode(buf_t *bp, size_t len, isns_value_t *value)
+isns_attr_type_nil_decode(__attribute__((unused))buf_t *bp, size_t len,
+		__attribute__((unused))isns_value_t *value)
 {
 	return len == 0;
 }
 
 static void
-isns_attr_type_nil_print(const isns_value_t *value, char *buf, size_t size)
+isns_attr_type_nil_print(__attribute__((unused))const isns_value_t *value,
+		char *buf, size_t size)
 {
 	snprintf(buf, size, "<empty>");
 }
 
 static int
-isns_attr_type_nil_parse(isns_value_t *value, const char *string)
+isns_attr_type_nil_parse(__attribute__((unused))isns_value_t *value,
+		const char *string)
 {
 	if (string && *string)
 		return 0;

client.c
@@ -55,7 +55,7 @@ isns_create_client(isns_security_t *ctx, const char *source_name)
 	sock = isns_create_bound_client_socket(
 			isns_config.ic_bind_address,
 			server_name,
-			"isns", 0, SOCK_STREAM);
+			"isns", AF_UNSPEC, SOCK_STREAM);
 	if (sock == NULL) {
 		isns_error("Unable to create socket for host \"%s\"\n",
 			isns_config.ic_server_name);

configure
@@ -3996,7 +3996,7 @@ _ACEOF
 esac
 
 if test "$GCC" = "yes"; then
-        CFLAGS="-Wall -Werror $CFLAGS"
+        CFLAGS="-Wall -Werror -Wextra $CFLAGS"
 	CPPFLAGS="$CPPFLAGS -D_GNU_SOURCE"
 fi
 

configure.ac
@@ -17,7 +17,7 @@ AC_PATH_PROG(SH, sh)
 dnl C Compiler features
 AC_C_INLINE
 if test "$GCC" = "yes"; then
-        CFLAGS="-Wall -Werror $CFLAGS"
+        CFLAGS="-Wall -Werror -Wextra $CFLAGS"
 	CPPFLAGS="$CPPFLAGS -D_GNU_SOURCE"
 fi
 

db.c
@@ -316,7 +316,7 @@ isns_db_get_next(isns_db_t *db,
 		isns_object_template_t *tmpl,
 		const isns_attr_list_t *current,
 		const isns_attr_list_t *scope,
-		const isns_source_t *source)
+		__attribute__((unused))const isns_source_t *source)
 {
 	return __isns_db_get_next(db->id_objects,
 			tmpl, current, scope);
@@ -326,8 +326,8 @@ isns_db_get_next(isns_db_t *db,
  * Get the search key tags
  */
 static int
-isns_db_get_key_tags(const isns_attr_list_t *keys,
-		uint32_t *tags, unsigned int max_tags)
+isns_db_get_key_tags(const isns_attr_list_t *keys, uint32_t *tags,
+		__attribute__((unused))unsigned int max_tags)
 {
 	unsigned int	i;
 
@@ -759,7 +759,7 @@ isns_db_expire(isns_db_t *db)
 		}
 
 		stamp += period;
-		if (stamp <= now) {
+		if ((time_t)stamp <= now) {
 			/* removing the object will move one
 			 * object from the tail to the free
 			 * slot in the list. So don't increment
@@ -770,7 +770,7 @@ isns_db_expire(isns_db_t *db)
 		} else {
 			isns_debug_state("Entity %u will expire in %u sec\n",
 					obj->ie_index, (int) (stamp - now));
-			if (stamp < next_timeout)
+			if ((time_t)stamp < next_timeout)
 				next_timeout = stamp;
 		}
 

dd.c
@@ -873,7 +873,7 @@ isns_dd_parse_attrs(isns_dd_t *dd, isns_db_t *db,
 			if (next->ia_tag_id != ISNS_TAG_DD_MEMBER_PORTAL_TCP_UDP_PORT)
 				goto invalid;
 			i += 1;
-			/* fallthru to normal case */
+			/* fallthru */
 
 		case ISNS_TAG_DD_MEMBER_PORTAL_INDEX:
 		case ISNS_TAG_DD_MEMBER_ISCSI_INDEX:
@@ -1083,7 +1083,9 @@ isns_dd_add_members(isns_dd_t *dd, isns_db_t *db, isns_dd_t *new_dd)
  * Remove members from a DD
  */
 int
-isns_dd_remove_members(isns_dd_t *dd, isns_db_t *db, isns_dd_t *temp_dd)
+isns_dd_remove_members(isns_dd_t *dd,
+		__attribute__((unused))isns_db_t *db,
+		isns_dd_t *temp_dd)
 {
 	isns_dd_member_t *mp;
 
@@ -1107,7 +1109,8 @@ isns_dd_remove_members(isns_dd_t *dd, isns_db_t *db, isns_dd_t *temp_dd)
 }
 
 void
-isns_dd_store(isns_db_t *db, const isns_dd_t *dd, int rewrite)
+isns_dd_store(__attribute__((unused))isns_db_t *db,
+		const isns_dd_t *dd, int rewrite)
 {
 	isns_object_t	*obj = dd->dd_object;
 	isns_dd_member_t *member;

esi.c
@@ -89,7 +89,7 @@ isns_esi_init(isns_server_t *srv)
  * Timer callback to send out ESI messages.
  */
 void
-isns_esi_transmit(void *ptr)
+isns_esi_transmit(__attribute__((unused))void *ptr)
 {
 	isns_db_t	*db = isns_esi_server->is_db;
 	isns_list_t	*esi_pos, *esi_next;
@@ -479,7 +479,9 @@ isns_process_esi(isns_server_t *srv, isns_simple_t *call, isns_simple_t **reply)
 }
 
 void
-isns_process_esi_response(uint32_t xid, int status, isns_simple_t *msg)
+isns_process_esi_response(uint32_t xid,
+		__attribute__((unused))int status,
+		isns_simple_t *msg)
 {
 	isns_portal_info_t	portal_info;
 	isns_esi_portal_t	*esp;
@@ -558,7 +560,8 @@ isns_esi_register(isns_object_t *obj)
  * We use this to keep track of ESI portals and such.
  */
 void
-isns_esi_callback(const isns_db_event_t *ev, void *ptr)
+isns_esi_callback(const isns_db_event_t *ev,
+		__attribute__((unused))void *ptr)
 {
 	isns_object_t	*obj, *entity;
 	isns_esi_t	*esi;

commit d3805876b641571152cecd654eb58bdca544b476

Fix broken server authentication initialization.
Commit 86bf736873ed cleaned up a bunch of compiler complaints,
and was supposed to have no effect on functionality. But the change
to isns_dsa_param_gen_callback() from void to returning an
integer (zero) broke "isnsd --init", causing the error message:

> Generating DSA parameters; this may take a while
> +
> Warning: Error generating DSA parameters - OpenSSL errors follow:

This commit changes isns_dsa_param_gen_callback() back to a void
function, and fixes the compiler problem by properly declaring
the callback, and using the proper openssl macro to set the
callback.

Fixes: 86bf736873ed General cleanup for the compiler.

pki.c
@@ -456,7 +456,7 @@ write_status_byte(int ch)
 	assert(res == 1);
 }
 
-static int
+static void
 isns_dsa_param_gen_callback(int stage,
 		__attribute__((unused))int index,
 		__attribute__((unused))void *dummy)
@@ -467,9 +467,6 @@ isns_dsa_param_gen_callback(int stage,
 		write_status_byte('.');
 	else if (stage == 2)
 		write_status_byte('/');
-
-	/* as a callback, we must return a value, so just return success */
-	return 0;
 }
 
 int
@@ -494,7 +491,7 @@ isns_dsa_init_params(const char *filename)
 	isns_notice("Generating DSA parameters; this may take a while\n");
 #if OPENSSL_VERSION_NUMBER >= 0x10002000L
 	cb = BN_GENCB_new();
-	BN_GENCB_set(cb, (int (*)(int, int, BN_GENCB *)) isns_dsa_param_gen_callback, NULL);
+	BN_GENCB_set_old(cb, (void (*)(int, int, void *)) isns_dsa_param_gen_callback, NULL);
 	dsa = DSA_new();
 	if (!DSA_generate_parameters_ex(dsa, dsa_key_bits, NULL, 0, NULL, NULL, cb)) {
 		DSA_free(dsa);